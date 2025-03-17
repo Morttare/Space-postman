5e19998dc0ce8d9c4eabb4676900a459358c9b7f
Morttare <max.jauhiainen@tuni.fi>
2025-03-17 15:36:11 +0200

    Very rough movement done

--- space-postman/Scenes/postman.gd
+++ space-postman/Scenes/postman.gd
@@ -4,22 +4,42 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var walk_speed = 4
+const TILE_SIZE = 16
+
+var initial_position = Vector2(0,0)
+var input_direction = Vector2(0,0)
+var is_moving = false
+var percent_moved_to_next_tile = 0.0
+
+
+func _ready():
+	initial_position = position
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_moving == false:
+		process_player_input()
+	elif input_direction != Vector2.ZERO:
+		move(delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		is_moving = false
 
-	move_and_slide()
+func process_player_input():
+	if input_direction.y == 0:
+		input_direction.x = int(Input.get_action_strength("ui_right")) - int(Input.get_action_strength("ui_left"))
+	if input_direction.x == 0:
+		input_direction.y = int(Input.get_action_strength("ui_down")) - int(Input.get_action_strength("ui_up"))
+	
+	if input_direction != Vector2.ZERO:
+		initial_position = position
+		is_moving = true
+		
+		
+func move(delta):
+	percent_moved_to_next_tile += walk_speed * delta
+	if percent_moved_to_next_tile >= 1.0:
+		position = initial_position + (TILE_SIZE * input_direction)
+		percent_moved_to_next_tile = 0.0
+		is_moving = false
+	else:
+		position = initial_position + (TILE_SIZE * input_direction * percent_moved_to_next_tile)
