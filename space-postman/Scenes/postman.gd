862e0ee73cff7eab4eedd1e8f9feaafd03ef495f
Morttare <max.jauhiainen@tuni.fi>
2025-03-18 10:08:21 +0200

    Updated movement

--- space-postman/Scenes/postman.gd
+++ space-postman/Scenes/postman.gd
@@ -4,13 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var walk_speed = 4
+var walk_speed = 400
 const TILE_SIZE = 16
 
 var initial_position = Vector2(0,0)
-var input_direction = Vector2(0,0)
 var is_moving = false
-var percent_moved_to_next_tile = 0.0
 
 
 func _ready():
@@ -19,27 +17,22 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	if is_moving == false:
 		process_player_input()
-	elif input_direction != Vector2.ZERO:
-		move(delta)
+		move_and_slide()
 	else:
 		is_moving = false
 
 func process_player_input():
-	if input_direction.y == 0:
-		input_direction.x = int(Input.get_action_strength("ui_right")) - int(Input.get_action_strength("ui_left"))
-	if input_direction.x == 0:
-		input_direction.y = int(Input.get_action_strength("ui_down")) - int(Input.get_action_strength("ui_up"))
+	var direction_horizontal := Input.get_axis("move_left", "move_right")
+	var direction_vertical := Input.get_axis("move_up", "move_down")
+		
+	if direction_horizontal:
+		velocity.x = direction_horizontal * walk_speed
 	
-	if input_direction != Vector2.ZERO:
-		initial_position = position
-		is_moving = true
-		
-		
-func move(delta):
-	percent_moved_to_next_tile += walk_speed * delta
-	if percent_moved_to_next_tile >= 1.0:
-		position = initial_position + (TILE_SIZE * input_direction)
-		percent_moved_to_next_tile = 0.0
-		is_moving = false
 	else:
-		position = initial_position + (TILE_SIZE * input_direction * percent_moved_to_next_tile)
+		velocity.x = move_toward(velocity.x, 0, walk_speed)
+		
+	if direction_vertical:
+		velocity.y = direction_vertical * walk_speed
+		
+	else:
+		velocity.y = move_toward(velocity.y, 0, walk_speed)
