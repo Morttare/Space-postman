e5a6e43e2eb345433288c98fddb4af7c9a4e7db8
KariNoHands <kari.soderqvist@tuni.fi>
2025-03-26 21:32:48 +0200

    Aggressive mainmenu people
    The SFX for starting the game now plays only once, even if the player keeps smashing the space button in vain hope for the game to start faster (thus re-starting the sfx again and again)

--- space-postman/mainmenu.gd
+++ space-postman/mainmenu.gd
@@ -1,7 +1,10 @@
 extends Control
 
+# For the space ship launch animation
 @onready var pathfollow : PathFollow2D = $Path2D/PathFollow2D
 
+# Because some fools smash space again and again
+@onready var justonce = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +16,8 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
 		$MenuMusic.stop()
 		pathfollow.progress_ratio = 1
-		$"MenuMusic/SFX StartGame".play()
+		if justonce == 0:
+			$"MenuMusic/SFX StartGame".play()
+			justonce += 1
 		await get_tree().create_timer(5).timeout
 		get_tree().change_scene_to_file.call_deferred("res://Scenes/knitted_planet.tscn")
