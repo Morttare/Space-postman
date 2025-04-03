2d845a566edaf922e437b49a2de6f64864ae6b0b
Terho Thorel <terho.thorel@outlook.com>
2025-04-03 19:23:29 +0300

    Changed background colour

--- space-postman/Scripts/kela_planet.gd
+++ space-postman/Scripts/kela_planet.gd
@@ -4,9 +4,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.current_planet = 1
-	#RenderingServer.set_default_clear_color("70899e")
-	RenderingServer.set_default_clear_color("324558")
-	#RenderingServer.set_default_clear_color("313131")
+	#RenderingServer.set_default_clear_color("70899e") #Light wall colour
+	#RenderingServer.set_default_clear_color("324558") #Dark wall colour
+	#RenderingServer.set_default_clear_color("313131") #Grey
+	RenderingServer.set_default_clear_color("1e1f22") #Dark grey
 	randomize()
 
 
